f1e56fb32c040f8dbb6ac2716630bea1377a1447
iceman1001 <iceman@iuse.se>
2015-04-17 09:47:21 +0200

    inital commit

---
+++ visualSequence/FrmMain.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace visualSequence
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutside = new System.Windows.Forms.Label();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab1 = new System.Windows.Forms.TabPage();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.tag2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tab1.SuspendLayout();
+            this.tag2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picBox1
+            // 
+            this.picBox1.BackColor = System.Drawing.Color.White;
+            this.picBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBox1.Location = new System.Drawing.Point(13, 8);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(640, 480);
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandom.Location = new System.Drawing.Point(578, 494);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 36);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "rand()";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 494);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Samples:";
+            // 
+            // lblOutside
+            // 
+            this.lblOutside.AutoSize = true;
+            this.lblOutside.Location = new System.Drawing.Point(27, 517);
+            this.lblOutside.Name = "lblOutside";
+            this.lblOutside.Size = new System.Drawing.Size(0, 13);
+            this.lblOutside.TabIndex = 3;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFolder.Location = new System.Drawing.Point(345, 21);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(34, 23);
+            this.btnFolder.TabIndex = 4;
+            this.btnFolder.Text = "...";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Location = new System.Drawing.Point(11, 21);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.Size = new System.Drawing.Size(328, 20);
+            this.tbFolder.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(17, 602);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(669, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(319, 74);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(73, 28);
+            this.btnLoad.TabIndex = 7;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab1);
+            this.tabControl1.Controls.Add(this.tag2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(706, 637);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tab1
+            // 
+            this.tab1.Controls.Add(this.btnGenerate);
+            this.tab1.Controls.Add(this.lblOutside);
+            this.tab1.Controls.Add(this.btnRandom);
+            this.tab1.Controls.Add(this.picBox1);
+            this.tab1.Controls.Add(this.label1);
+            this.tab1.Location = new System.Drawing.Point(4, 22);
+            this.tab1.Name = "tab1";
+            this.tab1.Padding = new System.Windows.Forms.Padding(3);
+            this.tab1.Size = new System.Drawing.Size(698, 611);
+            this.tab1.TabIndex = 0;
+            this.tab1.Text = "Image";
+            this.tab1.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(487, 494);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 36);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // tag2
+            // 
+            this.tag2.Controls.Add(this.groupBox1);
+            this.tag2.Location = new System.Drawing.Point(4, 22);
+            this.tag2.Name = "tag2";
+            this.tag2.Padding = new System.Windows.Forms.Padding(3);
+            this.tag2.Size = new System.Drawing.Size(698, 611);
+            this.tag2.TabIndex = 1;
+            this.tag2.Text = "Import";
+            this.tag2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbFolder);
+            this.groupBox1.Controls.Add(this.btnLoad);
+            this.groupBox1.Controls.Add(this.btnFolder);
+            this.groupBox1.Location = new System.Drawing.Point(15, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 143);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "The import file need to be in integer format, comma seperated.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 637);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Visual Sequence";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.tag2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOutside;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tab1;
+        private System.Windows.Forms.TabPage tag2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ visualSequence/FrmMain.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace visualSequence
+{
+    public struct seqChain
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
+
+    public partial class FrmMain : Form
+    {
+        private const int max_x = 639;
+        private const int max_y = 479;
+
+
+        private const int size_x = 640;
+        private const int size_y = 480;
+
+
+        private Bitmap bm = new Bitmap(1, 1);
+
+        private List<seqChain> chain = new List<seqChain>();
+        private List<seqChain> guesschain = new List<seqChain>();
+        private List<int> chainInts = new List<int>();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            Graphics bg = Graphics.FromImage(bm);
+            bg.Clear(Color.White);
+            bg.Dispose();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            lblOutside.Text = string.Empty;
+            if (chainInts.Any())
+            {
+                GenerateImage();
+                lblOutside.Text = chainInts.Count.ToString();
+            }
+            else
+            {
+                lblOutside.Text = "No values loaded.";
+            }
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            lblOutside.Text = string.Empty;
+            GenerateSeqNumsInts();
+            GenerateImage();
+            lblOutside.Text = chainInts.Count.ToString();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            var filename = tbFolder.Text;
+            LoadSeqNumsFromFile(filename);
+        }
+
+        private void GenerateImage()
+        {
+            Bitmap img = new Bitmap(size_x, size_y, PixelFormat.Format32bppRgb);
+
+            Graphics g = Graphics.FromImage(img);
+
+            //g.Clear(Color.Black);
+
+            int seq3th = 0, seq2th = 0, seq1th = 0;
+
+            double x1, y1, z1;
+            double pha = 0.2;
+            int x, y;
+
+            double usex = Math.Sin(pha);
+            double usey = Math.Cos(pha);
+
+            // Middle of sceen.
+            int half_x = (max_x/2);
+            int half_y = (max_y/2);
+            int outside = 0;
+
+            byte[] pixels = new byte[img.Height*img.Width];
+
+            foreach (int c in chainInts)
+            {
+                x1 = (c - seq1th)*(max_x/65535f/65535f);
+                y1 = (seq1th - seq2th)*(max_y/65535f/65535f);
+                z1 = (seq2th - seq3th)*(max_y/65535f/65535f);
+
+                x = (int) (x1*usey + z1*usex);
+                y = (int) (y1 + (z1*usey - x1*usex)/2);
+
+                x += half_x;
+                y += half_y;
+
+                outside++;
+
+                // Skip first 4 and make sure it is inside of picture
+                if (outside > 4 && x <= max_x && y <= max_y && x >= 0 && y >= 0)
+                {
+                    // transformera in i byte arrayn.
+                    int plats = x*y;
+
+                    if ((pixels[plats] + 5) < 255)
+                        pixels[plats] += 5;
+                    else
+                        pixels[plats] = 127;
+                }
+
+                // Switch up old seqences
+                seq3th = seq2th;
+                seq2th = seq1th;
+                seq1th = c;
+            }
+
+            LockUnlockBits(img, pixels);
+
+            picBox1.Image = img;
+            g.Dispose();
+        }
+
+        private void GenerateSeqNumsInts()
+        {
+            Random r = new Random();
+
+            chainInts = new List<int>();
+
+            for (int i = 0; i < 50000; i++)
+            {
+                chainInts.Add(r.Next(0, Int32.MaxValue));
+            }
+        }
+
+        private void LoadSeqNumsFromFile(string filename)
+        {
+            if (!File.Exists(filename))
+                return;
+
+            chainInts = new List<int>();
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string s = sr.ReadLine();
+                while (s != null)
+                {
+                    int a = Convert.ToInt32(s);
+                    chainInts.Add(a);
+                    s = sr.ReadLine();
+                }
+            }
+        }
+
+        private void LoadSeqChain()
+        {
+            for (int i = 3; i < chainInts.Count - 1; i++)
+            {
+                seqChain aChain = new seqChain();
+                aChain.x = chainInts[i] - chainInts[i - 1];
+                aChain.y = chainInts[i - 1] - chainInts[i - 2];
+                aChain.z = chainInts[i - 2] - chainInts[i - 3];
+
+                chain.Add(aChain);
+            }
+
+        }
+
+        private void Guess3d(int seqT_3, int seqT_2, int seqT_1, int radius)
+        {
+            int ny = seqT_1 - seqT_2;
+            int nz = seqT_2 - seqT_3;
+
+            lblOutside.Text = string.Format("Reconstructed: x={guess set} y={0} z={1} (radius={2})", ny, nz, radius);
+
+            guesschain = new List<seqChain>();
+
+            for (int i = 3; i < chain.Count - 1; i++)
+            {
+                int distance = Math.Abs(chain[i].z - nz) + Math.Abs(chain[i].y - ny);
+                if (distance <= radius)
+                {
+                    System.Diagnostics.Debug.WriteLine(
+                        string.Format("{0} {1}", distance, chain[i].x)
+                        );
+
+                    guesschain.Add(chain[i]);
+                }
+            }
+        }
+
+        private void LockUnlockBits(Bitmap bmp, byte[] imgValues)
+        {
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+            unsafe
+            {
+                // Get the address of the first line.
+                // IntPtr ptr = bmpData.Scan0;
+                //Sets the pointer to the first pixel
+                byte* ptr = (byte*) bmpData.Scan0;
+
+                int bytesPerPixel = BytesPerPixel(bmp.PixelFormat);
+                int remainOffset = bmpData.Stride - bmpData.Width*bytesPerPixel;
+
+                //Loops over each pixel
+                for (int y = 0; y < bmpData.Height; y++)
+                {
+                    for (int x = 0; x < bmpData.Width; x++)
+                    {
+                        byte colByte = imgValues[x*y];
+
+                        for (int pp = 0; pp < bytesPerPixel; pp++)
+                        {
+                            ptr[pp] = colByte;
+                        }
+
+                        ptr += bytesPerPixel;
+                    }
+                    //Corrects for the potential offset
+                    ptr += remainOffset;
+                }
+            }
+
+            // Unlock the bits.
+            bmp.UnlockBits(bmpData);
+        }
+
+        public static int BytesPerPixel(PixelFormat format)
+        {
+            switch (format)
+            {
+                case PixelFormat.Format16bppRgb555:
+                case PixelFormat.Format16bppRgb565:
+                case PixelFormat.Format16bppGrayScale:
+                    return 2;
+
+                case PixelFormat.Format24bppRgb:
+                    return 3;
+
+                case PixelFormat.Format32bppRgb:
+                case PixelFormat.Format32bppArgb:
+                case PixelFormat.Format32bppPArgb:
+                    return 4;
+
+                case PixelFormat.Format48bppRgb:
+                    return 6;
+
+                case PixelFormat.Format64bppArgb:
+                case PixelFormat.Format64bppPArgb:
+                    return 8;
+
+                default:
+                    throw new ArgumentNullException("format");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                tbFolder.Text = openFileDialog1.FileName;
+        }
+
+    }
+}
